5897bc8b92d1bf7118ca5c0b6e7d98dcf41bbcfa
Aybars <57864464+gunruy@users.noreply.github.com>
2025-03-19 09:50:23 +0300

    a
    a

--- Assets/Scripts/Player/CameraMovement.cs
+++ Assets/Scripts/Player/CameraMovement.cs
@@ -44,5 +44,5 @@ public class CameraMovement : MonoBehaviour
 }
 public class PlayerInputHandler : MonoBehaviour
 {
-    // Bu sýnýfýn içeriðini ihtiyacýnýza göre doldurun
+    // Bu sýnýfýn içeriðini ihtiyacýnýza göre doldurulmasý gerekiyor unutma!
 }
